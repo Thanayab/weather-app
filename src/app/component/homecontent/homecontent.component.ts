a98853221c8d85e548ca852c956bbbce2c04f4f0
Thanayab <thanaya.b@robosoftin.com>
2025-01-02 19:08:37 +0530

    updated

--- src/app/component/homecontent/homecontent.component.ts
+++ src/app/component/homecontent/homecontent.component.ts
@@ -12,10 +12,39 @@ import { CommonModule } from '@angular/common';
 export class HomecontentComponent implements OnInit {
   data:any={};
   b:boolean=false
+  currentTemperature: number=0; // current temperature in selected unit
+  currentUnit: string = 'C'; // default unit is Celsius
+  isCelsiusSelected: boolean = true;
+  formattedTime: string = '';
   ngOnInit(): void {
     this.data=JSON.parse(localStorage.getItem('weatherData')||'{}');
+    this.currentTemperature = this.data.current.temp_c;
 
   }
+
+
+  setTemperatureInCelsius() {
+    // Set the temperature initially in Celsius
+    this.currentTemperature = this.data.current.temp_c;
+    this.currentUnit = 'C';
+  }
+ 
+  setTemperatureInFahrenheit() {
+    // Convert temperature to Fahrenheit
+    this.currentTemperature = (this.data.current.temp_c * 9/5) + 32;
+    this.currentUnit = 'F';
+  }
+ 
+  convertToCelsius() {
+    this.isCelsiusSelected = true;
+    this.setTemperatureInCelsius();
+  }
+ 
+  convertToFahrenheit() {
+    this.isCelsiusSelected = false;
+    this.setTemperatureInFahrenheit();
+  }
+
   addToFavourite(){
     
     const favouriteList = JSON.parse(localStorage.getItem('favourite') || '[]');
